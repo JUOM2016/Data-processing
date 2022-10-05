3370d91ea140780cb93dae156e585cf40b72f795
Wei Jiang <sjiangwei@hotmail.com>
2022-10-05 13:24:52 +0100

    Wei

--- 20221005_T2_cal_calibration.py
+++ 20221005_T2_cal_calibration.py
@@ -30,7 +30,37 @@ echo_reading_width=3e-6
 ave=20
 steps=20
 
-for index in enumerate(sub_folders):
-    path=dir + '\\' + folder + '\\' + sub_folders[index] + '\\'
+for index , file in enumerate(sub_folders):
+    path=dir + '\\' + folder + '\\' + file + '\\'
+    file_list = os.listdir(path)
+    list_csv=natsorted([i for i in file_list if i.endswith('.CSV')])
+    file_size=np.shape(list_csv)
+    data_file_len=file_size[0]
+    read_example=pd.read_csv(os.path.join(path,list_csv[0]))
+    read_example=read_example.to_numpy()
+    row_of_data, column_of_data=np.shape(read_example)
 
+    tau_measured=np.empty(data_file_len)
+    echo_area=np.empty(steps*ave)
+    tau_plot=np.empty(steps)
+    data_time=np.empty([row_of_data,data_file_len])
+    data_trigger=np.empty([row_of_data,data_file_len])
+    data_echo=np.empty([row_of_data,data_file_len])
+    data_echo_corrected=np.empty([row_of_data,data_file_len])
+    #data_background=np.empty([row_of_data,data_file_len])
+    data_background_ave=np.empty(steps*ave)
+    Trigger_pos=np.empty([2,data_file_len])
+    pulses_pos=np.empty([4,data_file_len])
+
+    # Define the fitting fuction
+    def TPE(x,y0,T2):
+        return y0*np.exp(-4*x/T2)
+
+    # Set up the time range for data processing
+    time_offset_left=10
+    time_offset_right=180
+
+    # Values for finding the trigger, pi pulse, and pi/2 pulses
+    trigger_val = 1.5
+    pulses_val = 0.007
     test=0
